7f8eff1f958f5ddb98f067a373fac061d19cffcd
chehin <chehinjuan@gmail.com>
2022-07-10 22:07:56 -0300

    modificacion logo + name

--- app-movil/RestApp.Android/MainActivity.cs
+++ app-movil/RestApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace RestApp.Droid
 {
-    [Activity(Label = "Bumam", Icon = "@mipmap/bumamlogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,ScreenOrientation =ScreenOrientation.Landscape)]
+    [Activity(Label = "Sisgeres", Icon = "@mipmap/sisgeres", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,ScreenOrientation =ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
