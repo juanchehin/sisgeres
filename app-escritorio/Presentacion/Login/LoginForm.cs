65a80f3a388d64a13ed841ecba1ab0d1426234a5
chehin <chehinjuan@gmail.com>
2022-09-18 17:19:15 -0300

    modifi name project

--- app-escritorio/Presentacion/Login/LoginForm.cs
+++ app-escritorio/Presentacion/Login/LoginForm.cs
@@ -43,7 +43,6 @@ namespace sisgeres.Presentacion.Login
             {
                 if (contadorconexion > 0)
                 {
-
                     centralPaneles();
                     dibujarUsuarios();
                     ObtenerIpLocal();
