98340c53817a7a85392c34a2b865f935801d1fef
juanchehin <chehinjuan@gmail.com>
2024-06-24 19:01:53 -0300

    add linea and file in logs

--- app-escritorio/libs/LogsCustom.cs
+++ app-escritorio/libs/LogsCustom.cs
@@ -1,51 +1,54 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 
 namespace sisgeres.libs
 {
     public class LogsCustom
     {
-        public void alta_log(string mensaje)
+        public void alta_log(string mensaje,
+                         [CallerFilePath] string filePath = "",
+                         [CallerMemberName] string memberName = "",
+                         [CallerLineNumber] int lineNumber = 0)
         {
             try
             {
-
                 string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
                 string date = DateTime.Now.ToString("yyyy-MM-dd");
-                string fileName = $"log_{date}.txt";
-                string filePath = Path.Combine(appDataFolder, "sisgeres", fileName);
+                string logFileName = $"log_{date}.txt";
+                string logFilePath = Path.Combine(appDataFolder, "sisgeres", logFileName);
 
                 // Verifica si el directorio del archivo existe, si no, lo crea
-                string directoryPath = Path.GetDirectoryName(filePath);
+                string directoryPath = Path.GetDirectoryName(logFilePath);
                 if (!Directory.Exists(directoryPath))
                 {
                     Directory.CreateDirectory(directoryPath);
                 }
 
                 // Verifica si el archivo existe, si no, lo crea y escribe contenido inicial
-                if (!File.Exists(filePath))
+                if (!File.Exists(logFilePath))
                 {
-                    using (StreamWriter writer = File.CreateText(filePath))
+                    using (StreamWriter writer = File.CreateText(logFilePath))
                     {
                         string mensaje_creacion = "Este es un nuevo archivo creado.";
-                        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); // Obtiene la fecha y hora actual
+                        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                         writer.WriteLine($"[{timestamp}] - {mensaje_creacion}");
                     }
                 }
 
                 // Escribir el mensaje en el archivo
-                using (StreamWriter writer = new StreamWriter(filePath, true))
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
                 {
-                    string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); // Obtiene la fecha y hora actual
-                    writer.WriteLine($"[{timestamp}] - {mensaje}");
+                    string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    writer.WriteLine($"[{timestamp}] - {mensaje} (File: {Path.GetFileName(filePath)}, Member: {memberName}, Line: {lineNumber})");
                 }
             }
             catch (Exception ex)
             {
-                //MessageBox.Show(ex.Message.ToString(), "Error al crear el archivo logs");
+                // Manejar excepciones si es necesario
+                Console.WriteLine($"Error: {ex.Message}");
             }
-
         }
 
     }
